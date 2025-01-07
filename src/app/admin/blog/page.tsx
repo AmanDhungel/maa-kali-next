4d06daefd6bbff90ce108a39e3d124b2cca1959c
AmanDhungel <80142075+AmanDhungel@users.noreply.github.com>
2025-01-07 20:19:00 +0545

    completed maa kali

--- src/app/admin/blog/page.tsx
+++ src/app/admin/blog/page.tsx
@@ -1,9 +1,11 @@
-import BackendBlog from "@/components/BackendBlog";
 import { Metadata } from "next";
+import dynamic from "next/dynamic";
 
 export const metadata: Metadata = {
   title: "Maa Kali Hardware X National Home Decor",
   description: "Hardware near you, Construction company",
 };
+
+const BackendBlog = dynamic(() => import("@/components/BackendBlog"));
 
 export default BackendBlog;
--- src/app/admin/product/page.tsx
+++ src/app/admin/product/page.tsx
@@ -1,10 +1,10 @@
-import BackendProduct from "@/components/BackendProduct";
-
 import { Metadata } from "next";
+import dynamic from "next/dynamic";
 
 export const metadata: Metadata = {
   title: "Maa Kali Hardware - Product",
   description: "Hardware near you, Construction company",
 };
+const BackendProduct = dynamic(() => import("@/components/BackendProduct"));
 
 export default BackendProduct;
